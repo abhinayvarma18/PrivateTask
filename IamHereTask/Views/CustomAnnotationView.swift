c5fc80c658ef2a2a965ce544e57eed6190b0ef20
ABHINAY <abhinay.varma@housejoy.in>
2018-04-12 14:45:45 +0530

    Folder Structuring

---
+++ IamHereTask/Views/CustomAnnotationView.swift
@@ -0,0 +1,13 @@
+//
+//  CustomAnnotationView.swift
+//  IamHereTask
+//
+//  Created by ABHINAY on 12/04/18.
+//  Copyright © 2018 ABHINAY. All rights reserved.
+//
+
+import UIKit
+
+class CustomAnnotationView: UIView {
+
+}
